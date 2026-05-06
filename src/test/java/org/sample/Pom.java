60f3bad4779e58242c3d21ac56f615e3e75b51df
sathya11042000 <sathyastudy2000@gmail.com>
2026-05-06 12:27:10 +0530

    B msg

---
+++ src/test/java/org/sample/Pom.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Pom {
+
+	@FindBy(name = "email")
+	public WebElement user;
+
+}
